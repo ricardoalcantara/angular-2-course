aaab822675aece9aaba18beb69c928052288f2d3
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2017-03-18 02:00:43 -0300

    Updates do curso

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe= new Recipe('Dummy', 'Dummy', 'http://thumbs1.ebaystatic.com/d/l225/m/mM5u2kVAYbs3IRlwPfl6uOA.jpg');
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
 }
