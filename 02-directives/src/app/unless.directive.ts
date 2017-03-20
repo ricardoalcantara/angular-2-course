93c08da97fe51eb2dcb9af4a19a8f3f57d41fffc
Ricardo Alcantara <alcantarafox@yahoo.com.br>
2017-03-20 09:10:14 -0300

    Backup studies

---
+++ 02-directives/src/app/unless.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[dirUnless]'
+})
+export class UnlessDirective {
+  @Input('dirUnless') set unless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,12 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schintzel', 'Very tasty', 'https://media-cdn.tripadvisor.com/media/photo-s/05/0b/a5/7b/schintzel-con-patate.jpg', []),
+    new Recipe('Summer Salad', 'Okayish', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/beetroot-feta-grain-salad.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe= new Recipe('Dummy', 'Dummy', 'http://thumbs1.ebaystatic.com/d/l225/m/mM5u2kVAYbs3IRlwPfl6uOA.jpg');
+
   constructor() { }
 
   ngOnInit() {
